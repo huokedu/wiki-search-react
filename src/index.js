e8dde6cf5dfda2c751386d3dd787e18732f9926a
Thiago de Bastos <thiago@thiagodebastos.com>
2016-08-26 00:35:03 +1000

    initial commit :neckbeard:

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/SearchBar';
+
+const App = () => {
+	return <SearchBar />
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
