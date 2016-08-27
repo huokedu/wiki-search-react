231027e496f9bb86191e88306c64fa84efb1551d
Thiago de Bastos <thiago@thiagodebastos.com>
2016-08-28 00:40:43 +1000

    implement hasOwnProperty and ternary operator to render description

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -3,11 +3,13 @@ import React from 'react'
 // use article from props.article and delcare a new variable called article
 const ArticleListItem = ({ article }) => {
 
+	// ternary conditions can be used within JSX.
+	// check if article contains a prop called 'term', if so, render article.terms.description
 	return (
 		<li className="list-group-item">
 			<img src={article.thumbnail.source} height={article.thumbnail.height} width={article.thumbnail.width}/>
 			<div className="header">{article.title}</div>
-			{/* <div className="description">{article.terms["description"]}</div> */}
+			{article.hasOwnProperty('terms') ? <div className="description">{article.terms.description}</div> : null}
 		</li>
 	)
 }
